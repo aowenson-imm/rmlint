012b84a394f82ec5c7c2b71d7296d88b1f8d0fa0
aowenson-imm <andrew.owenson@imm.ox.ac.uk>
2026-03-12 12:55:01 +0000

    Traversal: skip folder if size < minsize

--- lib/traverse.c
+++ lib/traverse.c
@@ -339,6 +339,26 @@ static void rm_traverse_directory(RmTravBuffer *buffer, RmTravSession *trav_sess
         } else {
             switch(p->fts_info) {
             case FTS_D: /* preorder directory */
+                if(cfg->limits_specified && cfg->apply_limits_to_dirs && cfg->minsize > 0) {
+                    RmOff dir_size = (RmOff)p->fts_statp->st_size;
+                    if(dir_size < cfg->minsize) {
+                        /* skip folder if size < minsize
+                         * In some filesystems e.g. Ceph, folder size = 
+                         * recursive sum of all contents. */
+                        fts_set(ftsp, p, FTS_SKIP);
+                        g_atomic_int_inc(&trav_session->session->ignored_folders);
+                        clear_emptydir_flags = true;
+                        rm_log_debug_line(
+                            "Not descending into %s because size %" G_GINT64_FORMAT
+                            " is below minimum %" G_GINT64_FORMAT,
+                            p->fts_path,
+                            (gint64)dir_size,
+                            (gint64)cfg->minsize
+                        );
+                        break;
+                    }
+                }
+                
                 if(cfg->depth != 0 && p->fts_level >= cfg->depth) {
                     /* continuing into folder would exceed maxdepth*/
                     fts_set(ftsp, p, FTS_SKIP);  /* do not recurse */
